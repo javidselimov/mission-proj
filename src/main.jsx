32418e9ba18eda7161385cc3674a36ff5d532686
javidselimov12345 <javidzs@code.edu.az>
2023-05-25 12:03:51 +0400

    added changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { Provider } from "react-redux";
 import { store } from "./state/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store = {store}>
+ 
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+ 
 );
